ca40adbd0c355cc031689750c347761d90172f4a
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-02 15:29:05 +0300

    feat: add header

--- src/containers/Todo/Todo.jsx
+++ src/containers/Todo/Todo.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import classes from './Todo.module.css'
+import Header from '../../components/Header/Header'
 
 export default class Todo extends Component {
   render() {
     return (
       <div className={classes.TodoContainer}>
-        Todo container
+        <Header headerContent={'To do list'}/>
       </div>
     )
   }
