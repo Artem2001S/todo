582a9485a2a8f0c236b5d473f762b9cf2c8e0197
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-02 15:18:32 +0300

    feat: add container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import Todo from './containers/Todo/Todo'
 export default class App extends Component {
   render() {
     return (
       <div>
-        App
+        <Todo />
       </div>
     )
   }
---
+++ src/containers/Todo/Todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import classes from './Todo.module.css'
+
+export default class Todo extends Component {
+  render() {
+    return (
+      <div className={classes.TodoContainer}>
+        Todo container
+      </div>
+    )
+  }
+}
