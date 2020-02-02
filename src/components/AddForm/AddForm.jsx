bde6db70c7e9b5d95243673f48c734cafd25eff2
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-02 19:51:57 +0300

    feat: save AddForm value to state

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './AddForm.module.scss'
 
 export default function AddForm() {
+  const [value, setValue] = useState('');
+
   return (
-    <form onSubmit={(e) => e.preventDefault()} className={classes.AddForm}>
+    <form className={classes.AddForm} onSubmit={(e) => e.preventDefault()} >
       <div>
-        <input className={classes.Input} type="text" placeholder="What needs to be done ?" />
+        <input
+          className={classes.Input}
+          type="text"
+          placeholder="What needs to be done ?"
+          value={value}
+          onChange={(e) => { setValue(e.target.value); }}
+        />
       </div>
     </form>
   )
