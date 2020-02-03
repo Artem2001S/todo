34f10f7feccfb24e55d2e104c4ac10f9a78c224d
Artem2001S <39270533+Artem2001S@users.noreply.github.com>
2020-02-03 16:13:32 +0300

    Revert "Implement filters & Clear-completed button"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import Todo from './containers/Todo/Todo'
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Todo />
+        App
       </div>
     )
   }
