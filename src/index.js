a4e76721dc1f947b4a781026685d79fd3a8a9fd1
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-05 22:50:02 +0300

    saving redux store to localStorage

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux/rootReducer';
 
+const LOCAL_STORAGE_KEY = 'redux-store';
+
+const preLoadedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || undefined;
+
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   rootReducer,
+  preLoadedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 /* eslint-enable */
+
+store.subscribe(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.getState()));
+})
 
 const app = (
   <Provider store={store}>
