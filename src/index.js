09a3e4c14bcd2bde7275b27221d8d424b0fb578b
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-15 13:49:10 +0300

    refactor: move store to a separate file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './redux/rootReducer';
-
-const LOCAL_STORAGE_KEY = 'redux-store';
-
-const preLoadedState =
-  JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || undefined;
-
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  rootReducer,
-  preLoadedState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
-
-store.subscribe(() => {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.getState()));
-});
+import store from 'redux/store';
+import App from './App';
+import './index.scss';
 
 const app = (
   <Provider store={store}>
